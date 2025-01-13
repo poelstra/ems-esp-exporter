258a958408baaf05d5724c83f4cab0cd46a4c1e6
Martin Poelstra <martin@beryllium.net>
2025-01-13 14:16:11 +0100

    Add test for original (3.5) device values.

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -1,10 +1,16 @@
 import { expect, it } from "@jest/globals";
 import { Registry } from "prom-client";
-import { addSystemMetrics } from "./metrics";
-import { getExampleSystem } from "./test/examples";
+import { addDeviceMetrics, addSystemMetrics } from "./metrics";
+import { getExampleDeviceValues, getExampleSystem } from "./test/examples";
 
 it("handles system metrics", async () => {
     const registry = new Registry();
     addSystemMetrics(registry, getExampleSystem());
+    expect(await registry.metrics()).toMatchSnapshot();
+});
+
+it("handles original device metrics", async () => {
+    const registry = new Registry();
+    addDeviceMetrics(registry, await getExampleDeviceValues(), 8, 115);
     expect(await registry.metrics()).toMatchSnapshot();
 });
--- src/test/examples.ts
+++ src/test/examples.ts
@@ -1,10 +1,32 @@
-import { System, parseSystem } from "../api";
+import path = require("path");
+import { parseSystem, System } from "../api";
+import { Entities, Entity, readEntities, Value } from "../entities";
+
+export const ENTITIES_CSV_PATH = "config/dump_entities.csv";
+export const ENTITIES_CSV_ORIGINAL_PATH = "config/dump_entities.csv.20230128";
+
+export async function getParsedEntities(
+    csv_path: string = ENTITIES_CSV_PATH,
+): Promise<Entity[]> {
+    const entitiesPath = path.resolve(__dirname, "../..", csv_path);
+    return readEntities(entitiesPath);
+}
 
 export function getExampleSystem(): System {
     return parseSystem(JSON.parse(EXAMPLE_SYSTEM_RESPONSE));
 }
 
-const EXAMPLE_SYSTEM_RESPONSE = `{
+export async function getExampleDeviceValues(): Promise<Value[]> {
+    const entities = new Entities(
+        await getParsedEntities(ENTITIES_CSV_ORIGINAL_PATH),
+    );
+    return entities.parseValues(
+        115,
+        JSON.parse(EXAMPLE_DEVICE_ORIGINAL_RESPONSE),
+    );
+}
+
+export const EXAMPLE_SYSTEM_RESPONSE = `{
     "System Info": {
       "version": "3.5.1-dev.0",
       "platform": "ESP32",
@@ -138,4 +160,75 @@ const EXAMPLE_SYSTEM_RESPONSE = `{
         "entities": 0
       }
     ]
+  }`;
+
+export const EXAMPLE_DEVICE_ORIGINAL_RESPONSE = `{
+    "wwsettemp": 61,
+    "wwseltemp": 60,
+    "wwtype": "buffer",
+    "wwcomfort": "hot",
+    "wwflowtempoffset": 20,
+    "wwcircpump": "on",
+    "wwchargetype": "3-way valve",
+    "wwhyston": -11,
+    "wwhystoff": 0,
+    "wwdisinfectiontemp": 70,
+    "wwcircmode": "2x3min",
+    "wwcirc": "off",
+    "wwcurtemp": 43.3,
+    "wwcurtemp2": 58.9,
+    "wwcurflow": 0.0,
+    "wwstoragetemp1": 43.3,
+    "wwstoragetemp2": 58.9,
+    "wwactivated": "on",
+    "wwonetime": "off",
+    "wwdisinfecting": "off",
+    "wwcharging": "off",
+    "wwrecharging": "off",
+    "wwtempok": "on",
+    "wwactive": "off",
+    "ww3wayvalve": "off",
+    "wwstarts": 29519,
+    "wwworkm": 192694,
+    "heatingactive": "off",
+    "tapwateractive": "off",
+    "selflowtemp": 20,
+    "heatingpumpmod": 60,
+    "curflowtemp": 25.3,
+    "rettemp": 25.5,
+    "switchtemp": 0.0,
+    "syspress": 0.9,
+    "boiltemp": 24.1,
+    "burngas": "off",
+    "burngas2": "off",
+    "flamecurr": 0.0,
+    "heatingpump": "on",
+    "fanwork": "off",
+    "ignwork": "off",
+    "oilpreheat": "off",
+    "heatingactivated": "on",
+    "heatingtemp": 80,
+    "pumpmodmax": 100,
+    "pumpmodmin": 60,
+    "pumpmode": "proportional",
+    "pumpdelay": 12,
+    "burnminperiod": 10,
+    "burnminpower": 0,
+    "burnmaxpower": 80,
+    "boilhyston": -6,
+    "boilhystoff": 6,
+    "selburnpow": 80,
+    "curburnpow": 0,
+    "burnstarts": 136369,
+    "burnworkmin": 1184044,
+    "burn2workmin": 0,
+    "heatworkmin": 991350,
+    "heatstarts": 106850,
+    "ubauptime": 8939385,
+    "servicecode": "0Y",
+    "servicecodenumber": 204,
+    "maintenancemessage": "H00",
+    "maintenance": "off",
+    "maintenancetime": 6000,
+    "maintenancedate": "01.01.2004"
   }`;
